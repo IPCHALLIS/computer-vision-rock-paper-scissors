7991653ba43a5e0859521369f651044e74b374b6
Isaac Challis <isaac.challis@gmail.com>
2023-02-01 12:59:32 +0000

    Adds function call as it was not added initially.

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,6 @@
 import random
 
-labels = [rock, paper, scissors]
+labels = ['rock', 'paper', 'scissors']
 
 def get_computer_choice():
     computer_choice = random.choice(labels)
@@ -13,17 +13,18 @@ def get_user_choice():
 def get_winner():
     computer = get_computer_choice()
     user = get_user_choice()
-    if computer == rock and user == scissors:
+    if computer == 'rock' and user == 'scissors':
         print("You lost")
-    elif computer == paper and user == scissors:
+    elif computer == 'paper' and user == 'scissors':
         print("You won!")
-    elif computer == scissors and user == paper:
+    elif computer == 'scissors' and user == 'paper':
         print("You lost")
-    elif user == rock and computer == scissors:
+    elif user == 'rock' and computer == 'scissors':
         print("You won!")
-    elif user == paper and computer == scissors:
+    elif user == 'paper' and computer == 'scissors':
         print("You lost")
-    elif user == scissors and computer == paper:
+    elif user == 'scissors' and computer == 'paper':
         print("You won!")
     else:
         print("It is a tie!")
+get_winner()
