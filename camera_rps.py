65105465694e2ce0cec60cc10cfcdd9efc109e05
Isaac Challis <isaac.challis@gmail.com>
2023-02-03 14:37:39 +0000

    Changes the value on line 23 for normalise image.

--- camera_rps.py
+++ camera_rps.py
@@ -20,7 +20,7 @@ def get_prediction():
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        normalized_image = (image_np.astype(np.float32) / 127.5) - 1 # Normalize the image
         data[0] = normalized_image
         prediction = model.predict(data)
         cv2.imshow('frame', frame)
