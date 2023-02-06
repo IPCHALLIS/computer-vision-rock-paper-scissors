9c866529347212efd06919cecbca300e5ec30cda
Isaac Challis <isaac.challis@gmail.com>
2023-02-06 17:29:24 +0000

    Implementing 3 second timer in 'get_prediction()'

--- camera_rps copy.py
+++ camera_rps copy.py
@@ -20,8 +20,6 @@ def get_computer_choice():
 
 def get_prediction():
     while cap.isOpened():
-        end_after = 3
-        start = time.time()
         ret, frame = cap.read()
         if ret:
             frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -30,51 +28,39 @@ def get_prediction():
             data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
             data[0] = normalized_image
             cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1)
-            if key == ord('s'):
-                while True:
-                    timer = time.time() - start
-                    prediction = model.predict(data)
-                    index = np.argmax(prediction)
-                    class_name = class_names[index]
-                    confidence_score = prediction[0][index]
-                    print("Class:", class_name[:], end="")
-                    if timer >= end_after:
-                        return class_name
-                        break
+            prediction = model.predict(data)
+            index = np.argmax(prediction)
+            class_name = class_names[index]
+            confidence_score = prediction[0][index]
+            print("Class:", class_name[:], end="")
+            print("Confidence Score:", confidence_score)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return class_name
+            break
 
 
-# get_prediction()
+#get_prediction()
 
 
 def get_winner():
     computer = get_computer_choice()
+    print(computer)
     user = get_prediction()
     print(user)
     if computer == 'Rock' and user == class_names[2]:
         print("You lost")
-        print("Computer Chose: ", computer)
     elif computer == 'Paper' and user == class_names[2]:
         print("You won!")
-        print("Computer Chose: ", computer)
     elif computer == 'Scissors' and user == class_names[1]:
         print("You lost")
-        print("Computer Chose: ", computer)
     elif user == class_names[0] and computer == 'Scissors':
         print("You won!")
-        print("Computer Chose: ", computer)
     elif user == class_names[1] and computer == 'Scissors':
         print("You lost")
-        print("Computer Chose: ", computer)
     elif user == class_names[2] and computer == 'Paper':
         print("You won!")
-        print("Computer Chose: ", computer)
-    elif user == class_names[3]:
-        print("You lost")
-        print("You did not make a valid move!")
     else:
         print("It is a tie!")
-        print("Computer, also chose: ", computer)
 
 def play():
     get_winner()
