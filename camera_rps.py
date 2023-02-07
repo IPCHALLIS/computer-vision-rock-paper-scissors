99c7551bb126f82c1ff63fe430e296aa780ce53c
Isaac Challis <isaac.challis@gmail.com>
2023-02-07 17:48:46 +0000

    Adding rounds. Game stops after 3 rounds total.

--- camera_rps.py
+++ camera_rps.py
@@ -38,46 +38,84 @@ def get_prediction():
                     index = np.argmax(prediction)
                     class_name = class_names[index]
                     confidence_score = prediction[0][index]
-                    print("Class:", class_name[:], end="")
                     if timer >= end_after:
                         return class_name
-                        break
+
 
 
 # get_prediction()
 
 
 def get_winner():
+    computer_wins = 0
+    user_wins = 0
     computer = get_computer_choice()
     user = get_prediction()
-    print(user)
     if computer == 'Rock' and user == class_names[2]:
         print("You lost")
         print("Computer Chose: ", computer)
+        computer_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif computer == 'Paper' and user == class_names[2]:
         print("You won!")
         print("Computer Chose: ", computer)
+        user_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif computer == 'Scissors' and user == class_names[1]:
         print("You lost")
         print("Computer Chose: ", computer)
+        computer_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif user == class_names[0] and computer == 'Scissors':
         print("You won!")
         print("Computer Chose: ", computer)
+        user_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif user == class_names[1] and computer == 'Scissors':
         print("You lost")
         print("Computer Chose: ", computer)
+        computer_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif user == class_names[2] and computer == 'Paper':
         print("You won!")
         print("Computer Chose: ", computer)
+        user_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     elif user == class_names[3]:
         print("You lost")
         print("You did not make a valid move!")
+        computer_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
     else:
         print("It is a tie!")
         print("Computer, also chose: ", computer)
+        computer_wins += 1
+        user_wins += 1
+        print("Computer Score: ", computer_wins)
+        print("Your Score: ", user_wins)
+
+    return computer_wins, user_wins
 
 def play():
-    get_winner()
+    rounds = 1
+    while rounds <= 3:
+        for i in range(rounds, 4):
+            print()
+            print("Press 's' key to start the countdown: " )
+            print()
+            print("Round: ", rounds)
+            get_winner()
+            rounds += 1
+        else:
+            break
+
 
 play()
 
