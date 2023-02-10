e8f12a4d1ddf044dc2e743cd66ddbf48d68410cc
Isaac Challis <isaac.challis@gmail.com>
2023-02-10 18:31:36 +0000

    Changing the logic of get_winner function.

--- manual_rps.py
+++ manual_rps.py
@@ -14,17 +14,9 @@ def get_user_choice():
 
 
 def get_winner(computer_choice, user_choice):
-    if computer_choice == 'Rock' and user_choice == 'Scissors':
+    if ((computer_choice == 'Rock') and (user_choice == 'Scissors')) or ((computer_choice == 'Scissors') and (user_choice == 'Paper')) or ((computer_choice == 'Paper') and (user_choice == 'Rock')):
         print("You lost")
-    elif computer_choice == 'Paper' and user_choice == 'Scissors':
-        print("You won!")
-    elif computer_choice == 'Scissors' and user_choice == 'Paper':
-        print("You lost")
-    elif user_choice == 'Rock' and computer_choice == 'Scissors':
-        print("You won!")
-    elif user_choice == 'Paper' and computer_choice == 'Scissors':
-        print("You lost")
-    elif user_choice == 'Scissors' and computer_choice == 'Paper':
+    elif ((computer_choice == 'Paper') and (user_choice == 'Scissors')) or ((user_choice == 'Rock') and (computer_choice == 'Scissors')) or ((user_choice == 'Paper') and (computer_choice == 'Rock')):
         print("You won!")
     else:
         print("It is a tie!")
